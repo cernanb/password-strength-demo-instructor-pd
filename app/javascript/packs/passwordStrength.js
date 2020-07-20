eb2b4861fdbe43d9f20bd34676d4b8cf667999b1
cernanb <cernanb@gmail.com>
2020-07-20 10:35:14 -0700

    cache variables in js function

--- app/javascript/packs/passwordStrength.js
+++ app/javascript/packs/passwordStrength.js
@@ -1,6 +1,8 @@
 function passwordStrengthChecker() {
-  console.log('hello');
-  console.log('yes');
+  const passwordInput = document.getElementById('password');
+  const passwordStrengthMeter = document.getElementById(
+    'password-strength-meter'
+  );
 }
 
 document.addEventListener('DOMContentLoaded', () => {
